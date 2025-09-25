d7aa6ea2b021adf1c2dc25c8857524897c8a4973
Proger <davydovavic002@gmail.com>
2025-09-25 22:08:41 +0500

    chore: update websocket URLfor new web service

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     // --- WebSocket-соединение ---
     console.log("Попытка подключения к WebSocket серверу...");
     statusEl.html('Подключение к серверу...');
-    const socket = new WebSocket('wss://chessrad.onrender.com/ws');
+    const socket = new WebSocket('https://chessrad.onrender.com');
 
     socket.onopen = function() {
         console.log('Соединение с WebSocket установлено!');
